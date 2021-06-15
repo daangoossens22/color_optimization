ad03a531bb9b04da439e2de49141e18d52603bb3
Daan Goossens <daangoossens22@gmail.com>
2021-06-15 20:19:31 +0200

    script to calculated mse added

---
+++ mse_calc.py
@@ -0,0 +1,31 @@
+import cv2
+import sys
+
+if len(sys.argv) != 3:
+    print("Command should be of structure: python \{program name\} \{original image\} \{resulting image\}")
+else:
+    original = cv2.imread(sys.argv[1])
+    result = cv2.imread(sys.argv[2])
+    original2 = None
+    result2 = None
+    h_min = None
+    w_min = None
+
+    h_o, w_o, c_o = original.shape
+    h_r, w_r, c_r = result.shape
+    if (h_o != h_r or w_o != w_r):
+        h_min = min(h_o, h_r)
+        w_min = min(w_o, w_r)
+        original2 = cv2.resize(original, (w_min, h_min), interpolation = cv2.INTER_AREA)
+        result2 = cv2.resize(result, (w_min, h_min), interpolation = cv2.INTER_AREA)
+
+    diff = cv2.absdiff(original2, result2)
+    mse = diff^2
+    cv2.imshow('original', original2)
+    cv2.imshow('result', result2)
+    cv2.imshow('mse', mse)
+    mse_num = sum(cv2.sumElems(mse)) / (h_min * w_min * 3)
+    print("mean squared error: " + str(mse_num))
+    cv2.imwrite(sys.argv[2].split(".")[0] + "_mse.png", mse)
+    cv2.waitKey(0)
+
